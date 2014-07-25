cb61dfd99cedda425954adc24d45d2cc145ed507
tomatoKiller <fengmozhui@163.com>
2014-07-25 11:26:07 +0800

    v2

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,13 +3,33 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        if(version_compare(PHP_VERSION,'5.3.0','>')) {
-        	echo "string";
-        } else {
-        	echo "aaa";
-        }
+        // echo U('Admin/Index/index',Null, 'xml');
+        // echo '<br>';
+        $data = array(
+          'aa' => '1',
+          'bb' => '2'
+        );
+        // $data = 'OK';
+        // $this->ajaxReturn($data);
+        echo I('id').'<br>';
+        // echo I('id').'<br>';
     }
+
+    public function _empty() {
+      echo 'empty'.'<br>';
+    }
+
+    public function _before_index() {
+      echo 'before index<br>';
+    }
+
+    public function _after_index() {
+      echo '<br>after index';
+
+    }
+
     public function hello() {
-    	echo "hello world!";
+    	echo "hello world!<br>";
+
     }
 }
